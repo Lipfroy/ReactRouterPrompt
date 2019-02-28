49665bb68ac7b3a454f08a383c7acb8d4efb6e46
santhoshp <santhoshp.j4@gmail.com>
2019-02-28 15:52:32 -0600

    c;leanup -

--- src/components/common/QuillEditor.js
+++ src/components/common/QuillEditor.js
@@ -18,17 +18,12 @@ class QuillEditor extends Component {
       text: nextProps.data
     });
   }
-
-  componentDidMount() {
-    // Toolbar.addHandler('image', () => { alert(' new image uploaded')})
-    // console.log(this.quillRef);
-  }
   
   handleChange(value) {
     this.setState({
       text: value
     });
-    // console.log(value)
+    console.log(value)
     if(this.props.onChange) {
       this.props.onChange(value)
     }
@@ -37,9 +32,9 @@ class QuillEditor extends Component {
   imageHandler = (image, callback) => {
     const self = this;
     const editor = self.quillRef.getEditor();
-    // let fileInput = self.quillRef.getEditor().container.querySelector('input.ql-image[type=file]');
-    // if (fileInput == null) {
-    const fileInput = document.createElement('input');
+    let fileInput = editor.container.querySelector('input.ql-image[type=file]');
+    if (fileInput == null) {
+      fileInput = document.createElement('input');
       fileInput.setAttribute('type', 'file');
       fileInput.setAttribute('hidden', 'true');
       fileInput.setAttribute('accept', 'image/png, image/gif, image/jpeg, image/bmp, image/x-icon');
@@ -50,24 +45,21 @@ class QuillEditor extends Component {
           const sizeInMB = fileSize / 1024;
           if(self.currentSize + sizeInMB <= self.allowedSize) {
             self.currenstSize += sizeInMB;
-            var reader = new FileReader();
+            const reader = new FileReader();
             reader.onload = function (e) {
-              var range = editor.getSelection(true);
-              // editor.updateContents(editor.getContents().retain(range.index).delete(range.length).insert({ image: e.target.result }), "user");
+              const range = editor.getSelection(true);
               editor.setContents(editor.getContents().delete(range.length).insert({ image: e.target.result }), "user");
               editor.setSelection(range.index + 1, "silent");
               fileInput.value = "";
             };
             reader.readAsDataURL(fileInput.files[0]);
           } else {
-            alert("You exceeded the max size limit");
+            alert("You exceeded the max size limit");  // Can be replaced by the Custom Modal
           }
         }
       });
-      // this.container.appendChild(fileInput);
       editor.container.appendChild(fileInput);
-      // console.log(self.quillRef);
-    // }
+    }
     fileInput.click();
   }
 
@@ -81,8 +73,6 @@ class QuillEditor extends Component {
         'clean'
       ],
       handlers: {
-        // handlers object will be merged with default handlers object
-        // image: this.imageHandler
         image: this.imageHandler
       }
     }
@@ -105,7 +95,6 @@ class QuillEditor extends Component {
           formats={this.formats} 
           theme="snow"
           ref={(el) => this.quillRef = el}/>
-        
       </div>
     );
   }
